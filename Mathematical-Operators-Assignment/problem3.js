ff1c7b77437049c3a8a13c0f795936094d96715c
uzairansari11 <112272822+uzairansari11@users.noreply.github.com>
2022-09-04 23:34:35 +0530

    step-3

---
+++ Mathematical-Operators-Assignment/problem3.js
@@ -0,0 +1,10 @@
+
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+
+let sum = (one + 2 * two + 3 * three + 3 * four + 2 * five + 2 * six);
+console.log(sum);
