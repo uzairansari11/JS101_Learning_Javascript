2bf93c64507607f86d4d857187a3732a5f08c2c7
uzairansari11 <112272822+uzairansari11@users.noreply.github.com>
2022-09-04 23:24:19 +0530

    step-1

---
+++ Introduction-to-JS-Variables-and-Data-Types/problem4.js
@@ -0,0 +1,13 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+let name = "Uzair Ansari";
+let school_name = "Integral University";
+let grade = "A";
+let section = "DBM-1";
+let roll_no = "36";
+let maths = 70;
+let english = 80;
+let science = 70;
+
+console.log(name + "\n" + school_name + "\n" + grade + "\n" + section + "\n" + roll_no + "\n" + "maths", maths + "\n" + "english", english + "\n" + "science", science);
